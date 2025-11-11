7fd22e409cff5eab6d8bebdc47bd952decee97ff
Enzo Santoro <ez.santoro@gmail.com>
2025-11-11 16:03:58 -0300

    Ajustes

--- notes-frontend/vite.config.js
+++ notes-frontend/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // usar '/' em desenvolvimento e '/Portifolio/' em produção (para gh-pages)
-  base: process.env.NODE_ENV === 'production' ? '/Portifolio/' : '/'
+  // usar '/' em desenvolvimento e '/Portiflio/' em produção (para gh-pages)
+  base: process.env.NODE_ENV === 'production' ? '/Portiflio/' : '/'
 })
