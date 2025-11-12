513a30c2e8d5dda6d2ba89d2e19c346d241072aa
Enzo Santoro <ez.santoro@gmail.com>
2025-11-12 09:16:33 -0300

    fix imagens

--- notes-frontend/vite.config.js
+++ notes-frontend/vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // usar '/' em desenvolvimento e '/Portiflio/' em produção (para gh-pages)
-  base: import.meta.env.PROD ? '/Portiflio/' : '/'
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    // usar '/' em desenvolvimento e '/Portiflio/' em produção (para gh-pages)
+    base: mode === 'production' ? '/Portiflio/' : '/'
+  }
 })
